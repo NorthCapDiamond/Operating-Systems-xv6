5630c96281e5508cdfae622b8ff2b327b4a91a48
Drobysh Dmitry <90309201+NorthCapDiamond@users.noreply.github.com>
2023-09-16 21:11:58 +0300

    Add files via upload

---
+++ lab1/dump.c
@@ -0,0 +1,8 @@
+void dump(){
+  struct proc *p = myproc();
+  uint32 index = 2;
+  for(uint64* i = &(p->trapframe->s2); i <= &(p->trapframe->s11); i++){ 
+    printf("s%d, value: %d\n", index, *((uint32*) i));
+    index++;
+  }
+}
